b3a1464256552acc9f4867c9de3434a63af38f79
Raman Kumar <KUMARR8Q@novartis.net>
2025-02-27 12:39:01 +0530

    company change

--- Scripts/global.js
+++ Scripts/global.js
@@ -5,15 +5,18 @@ function LoadSpinner() {
 }
 
 function LoadComapnies() {
+	var scoutSapSoldToID = $("#scoutSapSoldToID");
+	var countryCode = GetParameterValues("countryCode");
 	var getcomppanytask = {
 		url:
-			'https://auf-solar-dev-westeurope-01.azurewebsites.net/api/solar/config/getCompanyInfo?countryCode=CA',
+			'https://auf-solar-dev-westeurope-01.azurewebsites.net/api/solar/config/getCompanyInfo?countryCode=' + countryCode,
 		method: 'GET',
 		timeout: 0
 	};
 	try {
 		$.ajax(getcomppanytask).done(function (response) {
-			console.log(response);
+			var companies = result = typeof response === "string" ? JSON.parse(response) : response;
+			console.log(companies);
 		})
 	} catch (e) {
 		console.log(e);
