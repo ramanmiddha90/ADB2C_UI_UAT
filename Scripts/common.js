b30e5bfaab95ca5366b4f37d7e458699961359d6
Raman Kumar <KUMARR8Q@novartis.net>
2025-04-23 10:28:54 +0300

    updated common.js

--- Scripts/common.js
+++ Scripts/common.js
@@ -18,7 +18,7 @@ function HandleTabEvents(tabIndex = 1) {
         $("#passwrod-tab").click(function (e) {
             e.preventDefault();                      // Stop default
             e.stopImmediatePropagation(); // Stop internal B2C logic
-            SetTabURL();
+            SetTabURL(PR_POLICY);
         });
     }
     else {
@@ -26,21 +26,27 @@ function HandleTabEvents(tabIndex = 1) {
         $("#home-tab").click(function (e) {
             e.preventDefault();                      // Stop default
             e.stopImmediatePropagation(); // Stop internal B2C logic
-            SetTabURL();
+            SetTabURL(PE_POLICY);
         });
     }
 }
-function SetTabURL() {
+function setQueryParam(value,defaultValue = "") {
+    if (value == "" || value == null || value == undefined) {
+        value = defaultValue
+    }
+    return value;
+}
+function SetTabURL(policy) {
     var queryparams = JSON.parse($("#queryparams").val());
-    var countryCode = queryparams.countryCode ?? "US";
+    var countryCode = setQueryParam(queryparams.countryCode, "US");
     var return_url = queryparams.return_url ?? "";
-    var regType = queryparams.regType ?? "V1";
+    var regType = setQueryParam(queryparams.regType, "V1");
     var clientId = queryparams.clientId ?? "";
     var redirect_uri = queryparams.redirect_uri ?? "";
 
     var queryparams = new URLSearchParams(window.location.search);
     if (queryparams.has("redirect_uri")) {
-        queryparams.set("p", PR_POLICY);
+        queryparams.set("p", policy);
         window.location.replace(window.location.origin + window.location.pathname + "?" + queryparams.toString())
     }
     else {
@@ -48,7 +54,7 @@ function SetTabURL() {
         var originURL = document.domain;
         var tenantName = originURL.replace(".b2clogin.com", "") + ".onmicrosoft.com";
         var passwordURL = "https://" + originURL + "/" + tenantName +
-                            "/oauth2/v2.0/authorize?p="+PR_POLICY+"&client_id=" + clientId +
+                            "/oauth2/v2.0/authorize?p=" + policy +"&client_id=" + clientId +
                             "&nonce=defaultNonce&redirect_uri=" + redirect_uri + "&scope=openid&response_type=id_token&UI_Locales=en&return_url=" + return_url +
                             "&countryCode=" + countryCode + "&regType=" + regType;
         window.location.replace(passwordURL);
