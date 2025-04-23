7d8f85950ef85ca2149811186c2718b6507d480d
Raman Kumar <KUMARR8Q@novartis.net>
2025-04-23 12:40:33 +0300

    signin js poingint to new passwrod reset policy

--- Scripts/signin.js
+++ Scripts/signin.js
@@ -104,7 +104,7 @@
                     $("#resetPassword").click(function (event) {
 
                         var queryparams = new URLSearchParams(window.location.search);
-                        queryparams.set("p", "B2C_1A_PWRESET");
+                        queryparams.set("p", "B2C_1A_SOLAR_PWRESET_LOGIN");
                         window.location.search = queryparams.toString();
                     });
 
